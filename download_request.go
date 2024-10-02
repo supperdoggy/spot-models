91be91749802b1df6372b5cef9e65ab830315b8b
Maksym Marchyshak <maksym.marchyshak@icloud.com>
2024-10-02 23:05:16 +0200

    initial commit

---
+++ download_request.go
@@ -0,0 +1,13 @@
+package models
+
+type DownloadQueueRequest struct {
+	ID        string `json:"id" bson:"_id"`
+	CreatorID int64  `json:"creator_id" bson:"creator_id"`
+
+	SpotifyURL string `json:"spotify_url" bson:"spotify_url"`
+	Name       string `json:"name" bson:"name"`
+	Active     bool   `json:"active" bson:"active"`
+
+	CreatedAt int64 `json:"created_at" bson:"created_at"`
+	UpdatedAt int64 `json:"updated_at" bson:"updated_at"`
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/supperdoggy/SmartHomeServer/music-services/models
+
+go 1.21.6
