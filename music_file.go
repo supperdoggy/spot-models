e7609a2177345e512d5e5ddc4a93c4f20d0191fc
Maksym Marchyshak <maksym.marchyshak@icloud.com>
2024-10-14 20:23:02 +0200

    created music file structure

---
+++ music_file.go
@@ -0,0 +1,12 @@
+package models
+
+type MusicFile struct {
+	ID        string `json:"id" bson:"_id"`
+	CreatorID int64  `json:"creator_id" bson:"creator_id"`
+
+	Title string `json:"title" bson:"title"`
+	Path  string `json:"path" bson:"path"`
+
+	CreatedAt int64 `json:"created_at" bson:"created_at"`
+	UpdatedAt int64 `json:"updated_at" bson:"updated_at"`
+}
