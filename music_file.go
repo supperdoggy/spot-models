63e62afdd0157c5e57f5081678f093249e91295b
Maksym Marchyshak <maksym.marchyshak@icloud.com>
2024-10-14 20:55:08 +0200

    added meta data

--- music_file.go
+++ music_file.go
@@ -4,8 +4,9 @@ type MusicFile struct {
 	ID        string `json:"id" bson:"_id"`
 	CreatorID int64  `json:"creator_id" bson:"creator_id"`
 
-	Title string `json:"title" bson:"title"`
-	Path  string `json:"path" bson:"path"`
+	Title    string         `json:"title" bson:"title"`
+	Path     string         `json:"path" bson:"path"`
+	MetaData map[string]any `json:"meta_data" bson:"meta_data"`
 
 	CreatedAt int64 `json:"created_at" bson:"created_at"`
 	UpdatedAt int64 `json:"updated_at" bson:"updated_at"`
