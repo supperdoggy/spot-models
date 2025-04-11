aab0ae22686e13f1859aa7f9e0860338cf5aec8d
Maksym Marchyshak <maksym.marchyshak@icloud.com>
2025-04-11 14:17:29 +0200

    error retry count

--- download_request.go
+++ download_request.go
@@ -7,8 +7,10 @@ type DownloadQueueRequest struct {
 	SpotifyURL string `json:"spotify_url" bson:"spotify_url"`
 	Name       string `json:"name" bson:"name"`
 	Active     bool   `json:"active" bson:"active"`
+	Errored    bool   `json:"errored" bson:"errored"`
 
-	CreatedAt int64 `json:"created_at" bson:"created_at"`
-	UpdatedAt int64 `json:"updated_at" bson:"updated_at"`
-	SyncCount int   `json:"sync_count" bson:"sync_count"`
+	CreatedAt  int64 `json:"created_at" bson:"created_at"`
+	UpdatedAt  int64 `json:"updated_at" bson:"updated_at"`
+	SyncCount  int   `json:"sync_count" bson:"sync_count"`
+	RetryCount int   `json:"retry_count" bson:"retry_count"`
 }
