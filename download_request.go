10ac2121267e46cbb90f75b996560d782597a16a
Maksym Marchyshak <maksym.marchyshak@icloud.com>
2025-05-01 20:20:04 +0200

    playlists migration implementation

--- download_request.go
+++ download_request.go
@@ -14,3 +14,15 @@ type DownloadQueueRequest struct {
 	SyncCount  int   `json:"sync_count" bson:"sync_count"`
 	RetryCount int   `json:"retry_count" bson:"retry_count"`
 }
+
+type PlaylistRequest struct {
+	ID         string `json:"id" bson:"_id"`
+	CreatorID  int64  `json:"creator_id" bson:"creator_id"`
+	SpotifyURL string `json:"spotify_url" bson:"spotify_url"`
+
+	Active  bool `json:"active" bson:"active"`
+	Errored bool `json:"errored" bson:"errored"`
+
+	CreatedAt int64 `json:"created_at" bson:"created_at"`
+	UpdatedAt int64 `json:"updated_at" bson:"updated_at"`
+}
