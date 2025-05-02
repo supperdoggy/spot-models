f2899696bd73de0f1761dee2d59206d41be10599
Maksym Marchyshak <maksym.marchyshak@icloud.com>
2025-05-02 19:58:55 +0200

    updated music file structure

--- music_file.go
+++ music_file.go
@@ -1,10 +1,13 @@
 package models
 
 type MusicFile struct {
-	ID        string `json:"id" bson:"_id"`
-	CreatorID int64  `json:"creator_id" bson:"creator_id"`
+	ID string `json:"id" bson:"_id"`
 
-	Title    string         `json:"title" bson:"title"`
+	Artist string `json:"artist" bson:"artist"`
+	Album  string `json:"album" bson:"album"`
+	Title  string `json:"title" bson:"title"`
+	Genre  string `json:"genre" bson:"genre"`
+
 	Path     string         `json:"path" bson:"path"`
 	MetaData map[string]any `json:"meta_data" bson:"meta_data"`
 
